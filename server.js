25f4926375ac57382eb812e0ff0b8a0b878c7796
milankatira <milankatira26@gmail.com>
2021-10-09 22:37:47 +0530

    initial

---
+++ server.js
@@ -0,0 +1,28 @@
+//require('dotenv').config()
+
+const express = require('express')
+
+const app = express()
+
+const ejs = require('ejs')
+
+const path = require('path')
+
+const expressLayout = require('express-ejs-layouts')
+
+const PORT = process.env.PORT || 3300
+
+//!set templates engine
+app.get('/', (req, res) => {
+    res.render('home')
+})
+
+app.use(expressLayout)
+
+app.set('views', path.join(__dirname, '/resources/views'))
+
+app.set('view engine', 'ejs')
+
+app.listen(PORT, () => {
+    console.log(`Listening on port ${PORT}`)
+})
