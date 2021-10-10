bcdd5b9f697a6e9429741fce9ed280201d45ba84
milankatira <milankatira26@gmail.com>
2021-10-10 14:23:21 +0530

    login and register

--- server.js
+++ server.js
@@ -12,19 +12,29 @@ const expressLayout = require('express-ejs-layouts')
 
 const PORT = process.env.PORT || 3300
 
-//!set templates engine
-app.get('/', (req, res) => {
-    res.render('home')
-})
+app.use(express.static('public'))
 
 app.use(expressLayout)
-
-//asset
-app.use(express.static('public'))
 
 app.set('views', path.join(__dirname, '/resources/views'))
 
 app.set('view engine', 'ejs')
+
+app.get('/', (req, res) => {
+    res.render('home')
+})
+
+app.get('/cart', (req, res) => {
+    res.render('customers/cart')
+})
+
+app.get('/login', (req, res) => {
+    res.render('auth/login')
+})
+
+app.get('/register', (req, res) => {
+    res.render('auth/register')
+})
 
 app.listen(PORT, () => {
     console.log(`Listening on port ${PORT}`)
